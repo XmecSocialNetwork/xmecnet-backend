bef7ea4f8464405bb54d28e323fa6542871a6bce
Siva R <sivagrenganathan+github@protonmail.com>
2018-06-06 00:04:01 +0530

    Update model user, add field name

--- xmecnet/common/models.py
+++ xmecnet/common/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=50, primary_key=True)
+    username = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=60)
     password = models.CharField(max_length=255)
     roll_no = models.CharField(max_length=15, blank=True, null=True)
     date_of_birth = models.DateField()
@@ -18,7 +19,8 @@ class User(models.Model):
                               blank=True, null=True)
 
     def __str__(self):
-        return '<{0} {1}>'.format(self.roll_no, self.name)
+        return '<{0}>'.format(self.username)
 
     class Meta:
         verbose_name_plural = 'Users'
+        ordering = ['username']
