889203eedaa2e6b89e0b096db3bd46ed79a95c77
Siva R <sivagrenganathan+github@protonmail.com>
2018-06-05 23:57:47 +0530

    Add model User

--- xmecnet/common/models.py
+++ xmecnet/common/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+    password = models.CharField(max_length=255)
+    roll_no = models.CharField(max_length=15, blank=True, null=True)
+    date_of_birth = models.DateField()
+
+    BRANCH_CHOICES = (
+            ('CS', 'Computer Science and Engineering'),
+            ('BME', 'Biomedical Engineering'),
+            ('EC', 'Electronics & Communication Engineering'),
+            ('EEE', 'Electrical Engineering'),
+        )
+
+    branch = models.CharField(max_length=3, choices=BRANCH_CHOICES,
+                              blank=True, null=True)
+
+    def __str__(self):
+        return '<{0} {1}>'.format(self.roll_no, self.name)
+
+    class Meta:
+        verbose_name_plural = 'Users'
