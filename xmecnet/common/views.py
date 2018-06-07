9df066d69cf48340c21eef97dbe1914425479940
Siva R <sivagrenganathan+github@protonmail.com>
2018-06-07 17:49:56 +0530

    Update views, fix typo

--- xmecnet/common/views.py
+++ xmecnet/common/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.hashers import make_password
 
-# Create your views here.
+from .models import User
+
+
+# To be removed.
+@csrf_exempt
+def register(request):
+    if request.method == "POST":
+        try:
+            data = request.body.decode("utf-8")
+            if not User.objects.filter(email=data['email']).exists():
+                try:
+                    user = User.objects.create(email=data['email'],
+                                               name=data['name'],
+                                               roll_no=data['roll_no'],
+                                               password=make_password(data['password']),
+                                               date_of_birth=data['dob'],
+                                               branch=data['branch'])
+
+                    
+                    return JsonResponse({'Success': True})
+
+                except Exception as e:
+                    return JsonResponse({'Error': 'Invalid data'},
+                                        status=400)
+
+            else:
+                return JsonResponse({'Error': 'User already registered'},
+                                    status=400)
+        except Exception as e:
+            return JsonResponse({'Error': 'Something unexpected happened'},
+                                status=500)
+    else:
+        return JsonResponse({'Error': 'Invalid request'}, status=405)
+
+# TODO: Write endpoint for login and to setSession.
