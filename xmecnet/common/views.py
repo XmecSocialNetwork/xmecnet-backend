f0721891c5761d0672530d86ec9efe145c4177db
Siva R <sivagrenganathan+github@protonmail.com>
2018-06-23 18:24:58 +0530

    Update views, changed date object to string

--- xmecnet/common/views.py
+++ xmecnet/common/views.py
@@ -4,26 +4,36 @@ from django.contrib.auth.hashers import make_password
 
 from .models import User
 
+import datetime
+import json
 
 # To be removed.
 @csrf_exempt
 def register(request):
     if request.method == "POST":
         try:
-            data = request.body.decode("utf-8")
+            data = json.loads(request.body.decode("utf-8"))
+            
+            if 'roll_no' not in data:
+                data['roll_no'] = None
+
             if not User.objects.filter(email=data['email']).exists():
                 try:
-                    user = User.objects.create(email=data['email'],
-                                               name=data['name'],
-                                               roll_no=data['roll_no'],
-                                               password=make_password(data['password']),
-                                               date_of_birth=data['dob'],
-                                               branch=data['branch'])
-
+                    user = User.create(email=data['email'],
+                                       name=data['name'],
+                                       password=make_password(data['password']),
+                                       dobday=data['dobday'],
+                                       dobmonth=data['dobmonth'],
+                                       dobyear=data['dobyear'],
+                                       branch=data['branch'],
+                                       roll_no = data['roll_no']
+                                     )
+            
                     
                     return JsonResponse({'Success': True})
 
                 except Exception as e:
+                    print(e)
                     return JsonResponse({'Error': 'Invalid data'},
                                         status=400)
 
@@ -31,6 +41,7 @@ def register(request):
                 return JsonResponse({'Error': 'User already registered'},
                                     status=400)
         except Exception as e:
+            print(e)
             return JsonResponse({'Error': 'Something unexpected happened'},
                                 status=500)
     else:
